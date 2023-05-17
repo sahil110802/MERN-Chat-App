8c0192fbc64d65ca2f8bc65573d8f066205c8c76
Sahil Sahu <imsahilsahu11@gmail.com>
2023-05-17 21:16:56 +0530

    server, chatpage, fetchchat basic

---
+++ backend/server.js
@@ -0,0 +1,23 @@
+const express=  require("express");
+const chats=require("./data/data");
+const app=express();
+const cors=require('cors')
+const dotenv=require("dotenv");
+dotenv.config();
+app.use(cors());
+app.get('/', (req,res)=>{
+    res.send("API is RUNNIng");
+});
+
+app.get('/api/chat',(req,res)=>{
+    res.send(chats);
+})
+
+app.get("/api/chat/:id", (req,res)=>{
+    const singleChat=chats.find((c)=> c._id === req.params.id);
+    res.send(singleChat);
+})
+
+
+const PORT= process.env.PORT || 5000;
+app.listen(PORT, console.log(`Server started on port ${PORT}`));
---
+++ frontend/src/Pages/Chatpage.jsx
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+const Chatpage = () => {
+  const [Chats,setChats]=useState([]);
+
+  const fetchChat= async()=>{
+
+    const {data}= await axios.get('http://localhost:5000/api/chat');
+    console.log(data);
+    setChats(data);
+  };
+
+  useEffect(() => {
+    fetchChat();
+  }, []);
+  
+  
+  return (
+    <div>
+      {Chats.map((chat)=>(<div key={chat._id}>{chat.chatName}</div>))}
+    </div>
+  )
+}
+export default Chatpage
