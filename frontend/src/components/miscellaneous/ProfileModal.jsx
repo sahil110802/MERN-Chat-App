1e8cade6bf6fe02e7d4f6e35ef480a2ad14fa2bc
Sahil Sahu <imsahilsahu11@gmail.com>
2023-06-06 21:02:43 +0530

    profileModal n sidedrawer

---
+++ frontend/src/components/miscellaneous/ProfileModal.jsx
@@ -0,0 +1,36 @@
+import { ViewIcon } from '@chakra-ui/icons';
+import { Button, IconButton, Image, Modal, ModalBody, ModalCloseButton, ModalContent, ModalFooter, ModalHeader, ModalOverlay, Text, useDisclosure } from '@chakra-ui/react';
+import React from 'react'
+
+const ProfileModal = ({user,children}) => {
+    const { isOpen, onOpen, onClose } = useDisclosure();
+  return (
+    <div>{
+        children?(<span onClick={onOpen}>{children}</span>):(
+            <IconButton
+            d={{base:"flex"}}
+            icon={<ViewIcon/>}
+            onClick={onOpen}/>
+        )
+    }
+
+
+      <Modal size="lg" isCentered isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent >
+          <ModalHeader display={"flex"} justifyContent={"center"}
+          fontFamily="Work-sans"
+          fontSize="40px"
+          >{user.name}</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody display={"flex"} flexDir={"column"} alignItems={"center"} justifyContent={"center"}>
+          <Image borderRadius="full" boxSize="200px" src={user.pic} alt={user.name}/>
+          <Text m={"20px"} fontSize={"30px"} fontFamily={"Work-sans"}>Email:{user.email}</Text>
+          </ModalBody>
+        </ModalContent>
+      </Modal>
+    </div>
+  )
+}
+
+export default ProfileModal
--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -1,20 +1,69 @@
-import { Box, Button, Tooltip } from "@chakra-ui/react";
+import { Avatar, Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Tooltip } from "@chakra-ui/react";
 import { useState } from "react";
-
+import {BellIcon, ChevronDownIcon} from "@chakra-ui/icons"
+import { ChatState } from "../../Context/ChatProvider";
+import ProfileModal from "./ProfileModal";
+import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 const SideDrawer = () => {
   const [search, setSearch] = useState();
   const [searchresult, setSearchresult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingchat, setLoadingchat] = useState();
+  const {user}=ChatState();
+  const history=useHistory();
+  const logoutHandler=()=>{
+    localStorage.removeItem("userInfo");
+    history.push('/');
+  }
   return (
     <div>
-    <Box>
+    <Box 
+    d="flex"
+    justifyContent="space-between"
+    alignItems="center"
+    bg="white"
+    w="100%"
+    p="5px 10px 5px 10px"
+    borderWidth="5px"
+    >
+    <div>
     <Tooltip label="Search users to chat" hasArrow placement="bottom-end">
     <Button variant="ghost">
     <i className="fa-solid fa-magnifying-glass"></i>
+    <Text d={{base:"none",md:"flex"}}>Search User</Text>
     </Button>
     </Tooltip>
+    
+    <Menu>
+    <MenuButton p={1}>
+    <BellIcon/>
+    </MenuButton>
+    </Menu>
+
+    <Menu>
+    <MenuButton as={Button}
+    rightIcon={<ChevronDownIcon/>}
+    ><Avatar size="sm" cursor="pointer" name={user.name} src={user.pic}/>
+    </MenuButton>
+    <MenuList>
+    <ProfileModal user={user}>
+    <MenuItem>My Profile</MenuItem>
+    </ProfileModal>
+    <MenuDivider/>
+    <MenuItem onClick={logoutHandler}>Log Out</MenuItem>
+
+    </MenuList>
+    </Menu> 
+    </div>
     </Box>
+    <div>
+    <Menu>
+  <MenuButton as={Button} >
+    Actions
+  </MenuButton>
+  
+</Menu>
+    </div>
     </div>
   )
 }
