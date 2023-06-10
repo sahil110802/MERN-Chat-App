561a457b0ea7d276d313428311aaefdd7ecc6350
Sahil Sahu <imsahilsahu11@gmail.com>
2023-06-10 23:27:29 +0530

    sidedrawer

---
+++ frontend/src/components/ChatLoading.jsx
@@ -0,0 +1,19 @@
+import { Skeleton, Stack } from '@chakra-ui/react'
+
+const ChatLoading = () => {
+  return (
+    <div>
+    <Stack>
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+  <Skeleton height='20px' />
+
+</Stack>
+    </div>
+  )
+}
+
+export default ChatLoading
--- frontend/src/components/miscellaneous/SideDrawer.jsx
+++ frontend/src/components/miscellaneous/SideDrawer.jsx
@@ -1,19 +1,76 @@
-import { Avatar, Box, Button, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Tooltip } from "@chakra-ui/react";
+import { Avatar, Box, Button, Drawer, DrawerBody, DrawerContent, DrawerHeader, DrawerOverlay, Input, Menu, MenuButton, MenuDivider, MenuItem, MenuList, Text, Toast, Tooltip, useDisclosure, useToast } from "@chakra-ui/react";
 import { useState } from "react";
 import {BellIcon, ChevronDownIcon} from "@chakra-ui/icons"
 import { ChatState } from "../../Context/ChatProvider";
 import ProfileModal from "./ProfileModal";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
+import axios from 'axios'
+import ChatLoading from "../ChatLoading";
+import UserListItem from "../userAvatar/UserListItem";
 const SideDrawer = () => {
   const [search, setSearch] = useState();
   const [searchresult, setSearchresult] = useState([]);
   const [loading, setLoading] = useState(false);
   const [loadingchat, setLoadingchat] = useState();
+
   const {user}=ChatState();
+
+  const toast=useToast();
+
   const history=useHistory();
+
+  const {isOpen,onOpen,onClose}=useDisclosure();
+
+  const accessChat=async(userId)=>{
+    try {
+      setLoadingchat(true)
+      const config={
+        headers:{
+          "Content-type":"application/json",
+          Authorization:`Bearer ${user.token}`,
+        }
+        const {data}=await axios.post('http://localhost:5000/api/chat',{userId},config);
+      }
+    } catch (error) {
+      
+    }
+  }
+
   const logoutHandler=()=>{
     localStorage.removeItem("userInfo");
     history.push('/');
+  }
+
+  const handleSearch=async()=>{
+    if (!search) {
+      toast({
+  title: 'Please enter some user.',
+  position: 'top-left',
+  status: 'warning',
+  duration: 3000,
+  isClosable: true,
+});return;
+    }
+
+    try {
+      setLoading(true);
+      const config={
+        headers:{
+          Authorization:`Bearer ${user.token}`,
+        }
+      }
+      const {data}=await axios.get(`http://localhost:5000/api/user?search=${search}`,config)
+      setLoading(false)
+      setSearchresult(data)
+    } catch (error) {
+      toast({
+  title: 'Failed to load users.',
+  position: 'top-left',
+  status: 'warning',
+  duration: 3000,
+  isClosable: true,
+})
+    }
   }
   return (
     <div>
@@ -28,7 +85,7 @@ const SideDrawer = () => {
     >
     <div>
     <Tooltip label="Search users to chat" hasArrow placement="bottom-end">
-    <Button variant="ghost">
+    <Button variant="ghost" onClick={onOpen}>
     <i className="fa-solid fa-magnifying-glass"></i>
     <Text d={{base:"none",md:"flex"}}>Search User</Text>
     </Button>
@@ -56,6 +113,36 @@ const SideDrawer = () => {
     </Menu> 
     </div>
     </Box>
+
+    <Drawer placement="left" onClose={onClose} isOpen={isOpen}>
+    <DrawerOverlay/>
+    <DrawerContent>
+    <DrawerHeader borderBottomWidth="1px">Search User</DrawerHeader>
+    <DrawerBody >
+    <Box display="flex" pb={2}>
+    <Input 
+    mr={2} 
+    placeholder="Search by name or email"
+    value={search}
+    onChange={(e)=>setSearch(e.target.value)}
+    >
+    </Input>
+    <Button onClick={handleSearch}>Go</Button>
+    </Box>
+    {
+      loading?<ChatLoading/>:(
+        searchresult?.map(user=>(
+          <UserListItem
+          key={user._id}
+          user={user}
+          handleFunction={()=>accessChat(user._id)}
+          />
+        ))
+      )
+    }
+    </DrawerBody>
+    </DrawerContent>
+    </Drawer>
     <div>
     <Menu>
   <MenuButton as={Button} >
